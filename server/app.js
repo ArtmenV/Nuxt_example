6a76b103845be083f9a317bda6118391d7d609ad
Artmen_V <viliutich.artem@gmail.com>
2019-12-01 13:28:21 +0700

    60_lesson

--- server/app.js
+++ server/app.js
@@ -1,7 +1,11 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const mongoose = require('mongoose')
+const passport = require('passport')
+const passportStrategy = require('./middleware/passport-strategy')
 const authRoutes = require('./routes/auth.routes')
+const postRoutes = require('./routes/post.routes')
+const commentRoutes = require('./routes/comment.routes')
 const keys = require('./keys')
 const app = express()
 
@@ -9,10 +13,15 @@ mongoose.connect(keys.MONGO_URI)
   .then(() => console.log('MongoDB connected...'))
   .catch(error => console.error(error))
 
+app.use(passport.initialize())
+passport.use(passportStrategy)
+
 app.use(bodyParser.urlencoded({extended: true}))
 app.use(bodyParser.json())
 
 
 app.use('/api/auth', authRoutes)
+app.use('/api/post', postRoutes)
+app.use('/api/comment', commentRoutes)
 
 module.exports = app
